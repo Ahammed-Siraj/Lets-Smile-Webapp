d02e15d19678b8e67f858e6f88e1a44114a4a04c
Ahammed-Siraj <ahmdsiraj.123@gmail.com>
2025-10-23 23:55:35 +0530

    updated

--- src/api.js
+++ src/api.js
@@ -1,5 +1,6 @@
-//export const API_BASE_URL = "http://localhost:5000/api";
-export const API_BASE_URL = "http://192.168.1.5:5000/api";
+// export const API_BASE_URL = "http://localhost:5000/api";
+export const API_BASE_URL = process.env.REACT_APP_API_URL;
+// export const API_BASE_URL = "http://192.168.1.5:5000/api";
 
 export function authHeader() {
   const token = localStorage.getItem("token");
